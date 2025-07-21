a42b9af9b2b430f71a91f974dae4a0ea550ce7f6
Raza Ali <razaalli.webdev@gmail.com>
2025-07-21 10:57:57 +0500

    fix the logo and height of rightside component

--- src/components/utils/HeaderLogo.jsx
+++ src/components/utils/HeaderLogo.jsx
@@ -3,10 +3,8 @@ import { images } from "../../assets/assets";
 
 const HeaderLogo = () => {
   return (
-    <div className="w-full my-3">
-      <div className="w-80 mx-auto rounded-tl-[35px] rounded-br-[35px] bg-primary-500 md:h-36 flex items-center justify-center">
-        <img src={images.logo} className="h-36" alt="" />
-      </div>
+    <div className="p-4">
+      <img className="w-32 md:w-40" src={images.headerLogo} alt="Logo" />
     </div>
   );
 };
--- src/pages/Entery.jsx
+++ src/pages/Entery.jsx
@@ -4,20 +4,19 @@ import { images } from "../assets/assets";
 import HeaderLogo from "../components/utils/HeaderLogo";
 
 const Entery = () => {
-
-  
-
   return (
     <div className="flex flex-col md:flex-row h-screen overflow-hidden">
       {/* Left Side */}
       <div className="md:w-1/2 w-full hidden md:flex flex-col justify-between bg-gray-100">
-        <div className="">
-          <img className="w-32 md:w-40" src={images.headerLogo} alt="Logo" />
-        </div>
         <HeaderLogo />
+        <div className="w-full my-3">
+          <div className="w-80 mx-auto rounded-tl-[35px] rounded-br-[35px] bg-primary-500 md:h-36 flex items-center justify-center">
+            <img src={images.logo} className="h-36" alt="" />
+          </div>
+        </div>
         <div className="flex justify-center items-center">
           <img
-            className="max-w-full h-auto object-cover"
+            className="max-w-full h-full object-cover"
             src={images.illustration}
             alt="Illustration"
           />
