a67636d2e7231c25d444271bf6dfbbb860fc6abf
Raza Ali <razaalli.webdev@gmail.com>
2025-07-21 16:32:01 +0500

    create the Reset password page

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -9,7 +9,7 @@ const ForgotPassword = () => {
   const [forgotPassword, { isLoading }] = useForgotPasswordMutation();
   return (
     <div className="flex h-screen flex-wrap flex-col md:flex-row">
-      <div className="md:w-1/2">
+      <div className="md:w-1/2 w-[90%]">
         <HeaderLogo />
         <div className="flex items-center flex-col justify-center">
           <div className="phoneIcon  bg-white shadow-xl h-32 w-32 rounded-full flex items-center justify-center shadow-gray-500 text-4xl md:text-7xl text-primary-500">
@@ -18,10 +18,10 @@ const ForgotPassword = () => {
           <h2 className="md:text-4xl mt-5 font-semibold mb-3">
             Forgot Password?
           </h2>
-          <p className="text-gray-400 md:text-lg">
+          <p className="text-gray-400 md:text-lg text-center md:text-left">
             Do not worry! We will help you recover your password.
           </p>
-          <form className="md:w-[55%]">
+          <form className="md:w-[55%] w-[90%]">
             <div className="field border-b-[1px] border-black my-2">
               <label className="block py-2 text-lg">Email</label>
               <input
--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { HiOutlineKey } from "react-icons/hi";
+import HeaderLogo from "../../components/utils/HeaderLogo";
+import { useResetPasswordMutation } from "../../redux/services/api";
+import RightSide from "../../components/utils/RightSide";
 
 const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+  const [resetPassword, { isLoading }] = useResetPasswordMutation();
+  return (
+    <div className="flex h-screen flex-wrap flex-col md:flex-row">
+      <div className="md:w-1/2 w-[90%]">
+        <HeaderLogo />
+        <div className="flex items-center flex-col justify-center">
+          <div className="phoneIcon  bg-white shadow-xl h-32 w-32 rounded-full flex items-center justify-center shadow-gray-500 text-4xl md:text-7xl text-primary-500">
+            <HiOutlineKey className="" />
+          </div>
+
+          <form className="md:w-[55%] w-[90%] my-5">
+            <div className="w-full ">
+              <input
+                type="password"
+                placeholder="New Password"
+                className="w-full border-[1px] text-center border-gray-300 my-7 outline-primary-500 py-3 px-2 text-xl"
+              />
+            </div>
+            <div className="w-full ">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                className="w-full border-[1px] text-center border-gray-300 my-7 outline-primary-500 py-3 px-2 text-xl"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full h-12 shadow-lg shadow-gray-400 bg-primary-500 font-medium hover:opacity-90 my-4"
+              disabled={isLoading}
+            >
+              {isLoading ? "Continue..." : "Continue"}
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="md:w-1/2 w-full md:block hidden">
+        <RightSide leftPath={"/verify-otp"} rightPath={"/reset-success"} />
+      </div>
+    </div>
+  );
 };
 
 export default ResetPassword;
