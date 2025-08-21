b8c7064037d34b7ca28e00688a90340579a4d114
Raza Ali <razaalli.webdev@gmail.com>
2025-08-21 13:24:16 +0500

    add the brands section marquee in the create post page

--- src/pages/posts/CreatePost.jsx
+++ src/pages/posts/CreatePost.jsx
@@ -3,6 +3,10 @@ import CreatePostForm from "../../components/sections/createPost/CreatePostForm"
 import WhyChooseUsUtility from "../../components/utils/WhyChooseUsUtility";
 import InpirationSectoin from "../../components/sections/createPost/InpirationSectoin";
 import BannerInFilter from "../../components/sections/filter/BannerInFilter";
+import BrandMarquee from "../../components/BrandMarquee";
+import brands from "../../assets/carLogos/brands";
+import { Link } from "react-router-dom";
+import { MdArrowOutward } from "react-icons/md";
 
 const CreatePost = () => {
   return (
@@ -11,6 +15,20 @@ const CreatePost = () => {
       <WhyChooseUsUtility />
       <InpirationSectoin />
       <BannerInFilter />
+      <div className="px-4 md:px-16 py-12 bg-[#F5F5F5]">
+        <div className="flex items-center justify-between">
+          <h2 className="md:text-4xl text-2xl font-semibold">
+            Explore Our Premium Brands
+          </h2>
+          <Link
+            to="/view-all-brands"
+            className="flex items-center gap-2 text-primary-500"
+          >
+            Show All Brands <MdArrowOutward />
+          </Link>
+        </div>
+        <BrandMarquee brands={brands} />
+      </div>
     </div>
   );
 };
