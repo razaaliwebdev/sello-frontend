cfa1b10474580ff706cdad72c5f7f8360796ecdc
Raza Ali <razaalli.webdev@gmail.com>
2025-08-21 12:56:02 +0500

    create the inpspiration section in the create post page

--- src/pages/posts/CreatePost.jsx
+++ src/pages/posts/CreatePost.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CreatePostForm from "../../components/sections/createPost/CreatePostForm";
+import WhyChooseUsUtility from "../../components/utils/WhyChooseUsUtility";
+import InpirationSectoin from "../../components/sections/createPost/InpirationSectoin";
 
 const CreatePost = () => {
   return (
     <div>
       <CreatePostForm />
+      <WhyChooseUsUtility />
+      <InpirationSectoin />
     </div>
   );
 };
