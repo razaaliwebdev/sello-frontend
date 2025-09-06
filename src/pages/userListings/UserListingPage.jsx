f0b9f169209f7677ffa3dfd179c7e2efce2c21e5
Raza Ali <razaalli.webdev@gmail.com>
2025-09-06 12:15:41 +0500

    add explore our premium brands section and marquee of brands

--- src/pages/userListings/UserListingPage.jsx
+++ src/pages/userListings/UserListingPage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import UserListingHero from "../../components/userListings/UserListingHero";
+import BrandMarquee from "../../components/BrandMarquee";
+import brands from "../../assets/carLogos/brands";
+import { Link } from "react-router-dom";
+import { GoArrowUpRight } from "react-icons/go";
 
 const UserListingPage = () => {
   return (
     <div>
       <UserListingHero />
+      <div className="py-5 px-10">
+        <div className=" flex items-center justify-between w-full">
+          <h2 className="md:text-3xl text-xl font-semibold">
+            Explore Our Premium Brands
+          </h2>
+          <Link
+            to={"/view-all-brands"}
+            className="flex items-center gap-2 text-lg"
+          >
+            Show All Brands <GoArrowUpRight />{" "}
+          </Link>
+        </div>
+        <BrandMarquee brands={brands} />
+      </div>
     </div>
   );
 };
