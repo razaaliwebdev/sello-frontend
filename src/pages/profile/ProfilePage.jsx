51fb6174a1d833a93868dff3380889bb5e0e160f
Raza Ali <razaalli.webdev@gmail.com>
2025-09-21 23:08:16 +0500

    add the cusotomer reviews

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ProfileHero from "../../components/sections/profile/ProfileHero";
 import OnlineBanner from "../../components/sections/profile/OnlineBanner";
 import ReachUsSection from "../../components/sections/profile/ReachUsSection";
+import CustomerReviews from "../../components/sections/carDetails/CustomerReviews";
 
 const ProfilePage = () => {
   return (
     <div>
       <ProfileHero />
       <OnlineBanner />
+      <CustomerReviews />
       <ReachUsSection />
     </div>
   );
