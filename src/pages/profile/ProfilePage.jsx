c1af4fe8a658615530bb9a080ca651206e162a8d
Raza Ali <razaalli.webdev@gmail.com>
2025-09-21 22:42:03 +0500

    create the OnlineBanner section in the profile page

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProfileHero from "../../components/sections/profile/ProfileHero";
+import OnlineBanner from "../../components/sections/profile/OnlineBanner";
 
 const ProfilePage = () => {
   return (
     <div>
       <ProfileHero />
+      <OnlineBanner />
     </div>
   );
 };
