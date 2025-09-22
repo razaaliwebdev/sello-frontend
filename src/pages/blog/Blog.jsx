4b9a64920bcb92ea13aeb08b2f08ae37003b07ce
Raza Ali <razaalli.webdev@gmail.com>
2025-09-22 16:12:55 +0500

    add the brands marquee

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import BlogsHeroSection from "../../components/sections/blogs/BlogsHeroSection";
+import brands from "../../assets/carLogos/brands";
+import { GoArrowUpRight } from "react-icons/go";
+import BrandMarquee from "../../components/BrandMarquee";
+import { Link } from "react-router-dom";
 
 const Blog = () => {
   return (
     <div>
       <BlogsHeroSection />
+      <div className="py-8 px-10">
+        <div className=" flex items-center justify-between w-full">
+          <h2 className="md:text-3xl text-xl font-semibold">
+            Explore Our Premium Brands
+          </h2>
+          <Link
+            to={"/view-all-brands"}
+            className="flex items-center gap-2 text-lg"
+          >
+            Show All Brands <GoArrowUpRight />{" "}
+          </Link>
+        </div>
+        <BrandMarquee brands={brands} />
+      </div>
     </div>
   );
 };
