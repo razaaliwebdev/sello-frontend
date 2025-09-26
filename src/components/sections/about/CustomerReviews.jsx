553cc8aded2ce07929a9f16347f945f694d8a488
Raza Ali <razaalli.webdev@gmail.com>
2025-09-26 17:19:17 +0500

    fix something in the CustomerReviews section in the about page

--- src/components/sections/about/CustomerReviews.jsx
+++ src/components/sections/about/CustomerReviews.jsx
@@ -29,35 +29,39 @@ const reviews = [
 
 const CustomerReviews = () => {
   return (
-    <div className="bg-[#E6E6E6] px-6 py-10 w-full">
-      <div className="max-w-full mx-auto space-y-8">
-        {reviews.map((review, index) => (
-          <div
-            key={index}
-            className="bg-[#E6E6E6] p-4 border-b border-gray-300 last:border-none"
-          >
-            {/* Header row */}
-            <div className="flex justify-between items-start">
-              <div>
-                <h3 className="font-bold text-lg md:text-xl">{review.name}</h3>
-                <p className="font-semibold text-lg md:text-xl">
-                  {review.product}
-                </p>
+    <div className="bg-[#272525]">
+      <div className="bg-[#E6E6E6] px-6 py-10 w-full rounded-tr-[50px]">
+        <div className="max-w-full mx-auto space-y-8">
+          {reviews.map((review, index) => (
+            <div
+              key={index}
+              className="bg-[#E6E6E6] p-4 border-b border-gray-300 last:border-none"
+            >
+              {/* Header row */}
+              <div className="flex justify-between items-start">
+                <div>
+                  <h3 className="font-bold text-lg md:text-xl">
+                    {review.name}
+                  </h3>
+                  <p className="font-semibold text-lg md:text-xl">
+                    {review.product}
+                  </p>
+                </div>
+                <div className="text-right">
+                  <div className="text-yellow-500 text-xl">★★★★★</div>
+                  <p className="text-sm md:text-base text-gray-700">
+                    {review.date}
+                  </p>
+                </div>
               </div>
-              <div className="text-right">
-                <div className="text-yellow-500 text-xl">★★★★★</div>
-                <p className="text-sm md:text-base text-gray-700">
-                  {review.date}
-                </p>
-              </div>
-            </div>
 
-            {/* Review text */}
-            <p className="mt-3 text-gray-800 text-sm md:text-base leading-relaxed">
-              {review.review}
-            </p>
-          </div>
-        ))}
+              {/* Review text */}
+              <p className="mt-3 text-gray-800 text-sm md:text-base leading-relaxed">
+                {review.review}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
