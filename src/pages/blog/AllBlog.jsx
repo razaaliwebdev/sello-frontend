e3843831c2243f5053e2c1c2c0fe96c2652cb72f
amingarage <your_email@example.com>
2025-10-02 11:36:10 +0500

    add the new latter section

--- src/pages/blog/AllBlog.jsx
+++ src/pages/blog/AllBlog.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BlogsHeroSection from "../../components/sections/blogs/BlogsHeroSection";
 import BlogPosts from "../../components/sections/blogs/allBlogs/BlogPosts";
+import NewsLatter from "../../components/utils/NewsLatter";
 
 const AllBlog = () => {
   return (
     <div>
       <BlogsHeroSection />
       <BlogPosts />
+      <NewsLatter />
     </div>
   );
 };
