1ca4ee027dcd4c49734514d04dabfdafaa440534
Raza Ali <166945306+razaaliwebdev@users.noreply.github.com>
2025-11-23 12:15:35 +0500

    Update api.js

--- src/redux/services/api.js
+++ src/redux/services/api.js
@@ -1,8 +1,8 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
-// const BASE_URL = import.meta.env.VITE_API_URL || "https://sello-backend.onrender.com/api";
+// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://sello-backend.onrender.com/api";
 
 export const api = createApi({
     reducerPath: "api",
